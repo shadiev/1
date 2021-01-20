1fe330417c15bc74f66ca9e2a732365b2fe2a6f1
Magomed <shadiev@inbox.ru>
2021-01-20 23:50:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+
+// alert ("Hello");
+// const result = confirm ('Are you here?');
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+// alert('Vam '+answer+' let');
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers [0] = prompt('Imya', '');
+// answers [1] = prompt('Фамилия?', '');
+// answers [2] = prompt('Сколько лет?', '');
+
+// // document.write(answers);
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5/6`);
+
+
+// const user = 'Maga';
+// alert(`Hi, ${user}!`);
+
+// const user = [];
+// user [0] = prompt('Как ваше имя?');
+// alert(`Hi, ${user}!`);
+// let id = Symbol ('id');
+// alert(id.description);
+// console.log(5%2);
+// console.log(2+2*2 !== "6");
+// const isChecked = true,
+//         isClose = false;
+
+// console.log(isChecked || !isClose;);
+
+// const numderOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//         count: numderOfFilms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+// };
+// const   a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', ''),
+//         c = prompt('Один из последних просмотренных фильмов?', ''),
+//         d = prompt('На сколько оцените его?', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
